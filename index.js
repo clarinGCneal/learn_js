67b62e0630d9354674cd02f9d36d56527049c2e0
GC Neal Clarin <gcnealchristian.clarin@g.msuiit.edu.ph>
2024-03-05 15:21:12 +0800

    Add index.html, index.js, and style.css files

---
+++ index.js
@@ -0,0 +1,85 @@
+//console.log(`Hello`);
+//console.log(`I like pizza!`);
+
+
+//window.alert(`This is an alert`);
+//window.alert(`Corrected`);
+
+
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I like pizza`;
+
+// This is a comment
+
+/*
+Multi
+line
+comment
+*/
+
+// variable = A container that stores a value.
+//              Behaves as id it were the value it contains
+
+// 1. declaration let x;
+// 2. assignment x = 100;
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// let firstName = "Nella";
+
+
+// console.log(typeof firstName);
+// console.log(`Your name is ${firstName}`)
+// console.log(`You are ${age} years old`);
+// console.log(`The price is $${price}`);
+// console.log(`Your gpa is: ${gpa}`);
+
+// Note strings can contain numbers but cannot use strings for numbers.
+
+// Boolean 
+
+// let online = true;
+
+// console.log(`Bro is online: ${online}`);
+
+
+// let fullName = "Neal Cal";
+// let age = 25;
+// let isStudent = true;
+
+// document.getElementById("p1").textContent = `Your name is ${fullName}`;
+// document.getElementById("p2").textContent = `You are ${age} years old`;
+// document.getElementById("p3").textContent = isStudent;
+
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 2;
+// let extraStudents = students % 3;
+
+// students += 1;
+// students -= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+
+// operator precedence
+/*
+1. parenthesis ()
+2. exponents
+3. multiplication & division & modulo
+4. addition & subtraction
+*/
+ 
